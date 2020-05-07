ca327e126e22f9e1549fe2437e3e34c095d97a1c
Andria177 <andriarzk@gmail.com>
2020-05-08 02:01:13 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :gossips do
+    resources :likes
+    resources :comments
+  end
+  resources :users, :cities
+  root 'gossips#index'
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
+
 end
